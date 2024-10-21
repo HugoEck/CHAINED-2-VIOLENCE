ac70ad0489c8ba35e0520b2454cc0bef0dbeac16
scottmerseburg <112166703+scottmerseburg@users.noreply.github.com>
2024-10-21 14:24:59 +0200

    Player_Weapon_System
    Implemented a weaponsystem that alows players to pickup weapons

---
+++ Assets/Scripts/Weapon/FloatingWeapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingWeapon : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    public float floatAmplitude = 0.5f;
+    public float floatSpeed = 1f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Rotate the weapon
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        // Floating effect
+        transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time * floatSpeed) * floatAmplitude, 0);
+    }
+}
