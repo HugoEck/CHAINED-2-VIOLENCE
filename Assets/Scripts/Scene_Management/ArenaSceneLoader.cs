73ddda530dfe42ecae02f87b351b9fdf5c99034e
Arn-Svenning <112516477+Arn-Svenning@users.noreply.github.com>
2024-10-22 11:04:39 +0200

    delete old Scene loader

--- Assets/Scripts/Scene_Management/ArenaSceneLoader.cs
+++ Assets/Scripts/Scene_Management/ArenaSceneLoader.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; // You need this for scene loading
 
-public class SceneLoader : MonoBehaviour
+public class ArenaSceneLoader : MonoBehaviour
 {
     // Public variable to store the name of the scene you want to load
     public string newSceneName;
