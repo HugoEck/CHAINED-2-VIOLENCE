b94d133990b44171ff5fb1e30c307bcc07bbcffb
Worsaa <jack.hellstaf@gmail.com>
2024-11-14 15:07:41 +0100

    UI Stuff

--- Assets/Scripts/UI_Scripts/Player UI/PlayerUIManager.cs
+++ Assets/Scripts/UI_Scripts/Player UI/PlayerUIManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerUIManager : MonoBehaviour
 {
     public static PlayerUIManager Instance;
 
-    [SerializeField] TMP_Text runDurationTimerText;
+    [SerializeField] TMP_Text timeOfArenaRunLengthText;
     [SerializeField] TMP_Text currentWaveText;
     [SerializeField] TMP_Text remainingEnemiesPerWaveText;
 
@@ -32,18 +33,42 @@ public class PlayerUIManager : MonoBehaviour
     void Update()
     {
         UpdateTextElementsPlayerUI();
-    }
-
-    void UpdateRunDurationTimer()
-    {
-        //Chained2ViolenceGameManager.Instance
-        // must worst in game manager for this.
+        UpdateRunDurationTimer();
     }
 
     private void UpdateTextElementsPlayerUI()
     {
-        remainingEnemiesPerWaveText.text = ("Remaining Enemies: " + WaveManager.ActiveEnemies);
-        currentWaveText.text = ("Wave: " + WaveManager.currentWave);
+        if (SceneManager.GetActiveScene().name == "LobbyScene")
+        {
+            remainingEnemiesPerWaveText.gameObject.SetActive(false);
+            currentWaveText.gameObject.SetActive(false);
+            timeOfArenaRunLengthText.gameObject.SetActive(false);
+
+
+        }
+        else         
+        {
+            currentWaveText.gameObject.SetActive(true);
+            timeOfArenaRunLengthText.gameObject.SetActive(true);
+            remainingEnemiesPerWaveText.gameObject.SetActive(true);
+            remainingEnemiesPerWaveText.text = "Remaining Enemies: " + WaveManager.ActiveEnemies;
+            currentWaveText.text = "Wave: " + (WaveManager.currentWave - 1);
+        }
+    }
+
+    void UpdateRunDurationTimer()
+    {
+        if (SceneManager.GetActiveScene().name == "Arena")
+        {
+            float elapsedTime = Chained2ViolenceGameManager.Instance.GetElapsedTimeInArena();
+            timeOfArenaRunLengthText.text = FormatTime(elapsedTime);
+        }
+    }
+    string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60F);
+        int seconds = Mathf.FloorToInt(time % 60F);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
 }
