c5eedeb22eb7ecd50cc5bad6e45200911ddbf271
ViggeGee <victorgustafsson65@gmail.com>
2024-11-21 16:41:13 +0100

    item prefab made

---
+++ Assets/Items/Item.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Material legendaryMaterial;
+    public Material rareMaterial;
+    public Material commonMaterial;
+
+    public TextMeshPro stat1;
+    public TextMeshPro stat2;
+    public TextMeshPro stat3;
+    public TextMeshPro stat4;
+
+    public int attackModifier = 0;
+    public int speedModifier = 0;
+    public int chainkModifier = 0;
+    public int healthkModifier = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
