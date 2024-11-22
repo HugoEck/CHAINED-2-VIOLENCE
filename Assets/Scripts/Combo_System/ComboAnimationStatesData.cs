cf8c3e31e5548077521a83b149670cb35d9afdfe
Arn-Svenning <112516477+Arn-Svenning@users.noreply.github.com>
2024-11-22 11:07:27 +0100

    Some combo scripts

---
+++ Assets/Scripts/Combo_System/ComboAnimationStatesData.cs
@@ -0,0 +1,24 @@
+
+
+public static class ComboAnimationStatesData
+{
+    #region COMBO_UNARMED
+
+    public static string[] combosInUnarmedState = { "UnarmedCombo1", "Combo2", "Combo3" };
+
+    public static string unarmedSubStateDefault = "Base Layer.Attack Combos.Unarmed Default";
+    public static string unarmedSubStateTank = "Base Layer.Attack Combos.Unarmed Tank";
+    public static string unarmedSubStateWarrior = "Base Layer.Attack Combos.Unarmed Warrior";
+    public static string unarmedSubStateRanged = "Base Layer.Attack Combos.Unarmed Ranged";
+    public static string unarmedSubStateSupport = "Base Layer.Attack Combos.Unarmed Support";
+
+    #endregion
+
+    #region COMBO_TWO_HANDED
+
+    public static string[] combosInTwoHandedState = { "TwoHandedCombo1", "Combo2", "Combo3" };
+
+    public static string twoHandedSubState = "Base Layer.Attack Combos.Two Handed";
+
+    #endregion
+}
